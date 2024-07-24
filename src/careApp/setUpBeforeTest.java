313196350a8b0f9c03e63be90c7b6db946799e80
Mohammad Alkeswani <mohammad.alkeswani@tsmesolutions.com>
2024-07-24 14:56:29 +0300

    AppiumTask

--- src/careApp/setUpBeforeTest.java
+++ src/careApp/setUpBeforeTest.java
@@ -34,31 +34,7 @@ import junit.framework.Assert;
 
 public class setUpBeforeTest {
 	
-	// ScreenShot
-		public String getScreenshot(String TestCaseName , WebDriver driver) throws IOException {
-			TakesScreenshot src = ((TakesScreenshot) driver);
-			
-			File SrcFile = src.getScreenshotAs((OutputType.FILE));
 
-			File Dest = new File (System.getProperty ("user.dir") + "//reports//" + TestCaseName + ".png");
-			
-	          FileUtils.copyFile (SrcFile,Dest);
-			return System.getProperty ("user.dir") + "//reports//" + TestCaseName + ".png";
-		} 
-		
-		//Create report
-		public static ExtentReports report() {
-
-			 String path = System.getProperty("user.dir") + "/reports/index.html";
-				ExtentSparkReporter reporter = new ExtentSparkReporter(path);
-				reporter.config().setReportName("Mohammad Alkeswani Report");
-				reporter.config().setDocumentTitle("Title Alkeswani");
-				ExtentReports extent = new ExtentReports();
-				extent.attachReporter(reporter);
-				extent.setSystemInfo("Tester", "Alkeswani");
-				return extent;
-			
-		}
 	
   public AndroidDriver driver;
   // setup to connect with server and emulator 
